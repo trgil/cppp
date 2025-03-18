8504e29d2ec7065b7936a0b97611a13d77287741
Gil Treibush <tkks085@gmail.com>
2025-03-18 15:44:29 +0200

    Add C test header files
    Signed-off-by: Gil Treibush <tkks085@gmail.com>

---
+++ tests/c_test_files/cppp_test_01.h
@@ -0,0 +1,20 @@
+
+
+// CPPP test struct - white space & LB test
+struct cpp_test_01 {
+
+    int test_field_1;
+    int test_field_2;
+
+    int\
+    test_field_3;
+    int\
+    test_field_4;
+
+    int         test_field_5;
+    int			test_field_6;
+
+
+
+
+};
---
+++ tests/c_test_files/cppp_test_02.h
@@ -0,0 +1,26 @@
+
+
+// CPPP test struct - comments
+struct cpp_test_02 {
+    int test_field_1; // Field 1
+    int test_field_2; // Field 2
+    int test_field_3; // Field 3
+
+    /* Testr fields: 4, 5 & 6
+     *
+
+     *
+     * Multi-line comment.
+     */
+    int test_field_4;
+    int test_field_5;
+    int test_field_6;
+
+    // A /* B */ // C
+
+    /*
+     * /
+     // Comment 2
+     *	/
+     */
+};
