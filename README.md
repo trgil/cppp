aadb1396fb05f4f4396af6a581602f7038e6821c
Gil Treibush <tkks085@gmail.com>
2025-03-18 20:02:32 +0200

    Add package setup information
    Add package setup and requirements configurations.
    Refactor README file.
    
    Signed-off-by: Gil Treibush

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# cppp
-Python C/C++ Pre-Processor Package
+# CPPP
+**Python C/C++ Pre-Processor Package.**
+
+ ![Author](https://img.shields.io/badge/Author-Gil_Treibush-brightgreen) ![License](https://img.shields.io/badge/License-MIT-blue.svg) ![Version](https://img.shields.io/badge/Version-1.0.0--alpha.1-violet)
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cppp',
+    version='1.0.0-alpha.1',
+    description='A Python package for preprocessing C/C++ code',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Gil Treibush',
+    url='https://github.com/trgil/cppp',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'setuptools>=42',  # Specify version if needed
+    ],
+    python_requires='>=3.6',
+)
