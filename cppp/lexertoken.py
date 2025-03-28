6e3b98a0000e17725eb8fe6c18edaeb5afa4ae3e
Gil Treibush <tkks085@gmail.com>
2025-03-28 14:48:32 +0300

    Add a Lexer Token class
    Signed-off-by: Gil Treibush

---
+++ cppp/lexertoken.py
@@ -0,0 +1,43 @@
+"""
+A Lexer Token class source: lexertoken.py
+
+Author:     Gil Treibush
+Version:    1.0.0-alpha.1
+License:    MIT License
+"""
+
+
+class LexerToken:
+    """
+    This class represents a single Lexer Token.
+    """
+
+    _val = ""
+
+    def __init__(self, start, val = None):
+        self._start = start
+        if val:
+            self._val = self._val + val
+
+    def __repr__(self):
+        return f"LexerToken({self._start}, {self._val})"
+
+    def __str__(self):
+        return f"{self._val}"
+
+    def __add__(self, other):
+        if isinstance(other, LexerToken):
+            self._val = self._val + other._val
+        elif isinstance(other, str):
+            self._val = self._val + other
+        else:
+            raise TypeError("Token added to non-supported type")
+
+    def __len__(self):
+        return len(self._val)
+
+    def __eq__(self, other):
+        if isinstance(other, LexerToken):
+            return self._val == other._val
+        else:
+            raise TypeError("Token compared to non-supported type")
